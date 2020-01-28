6523858b6820f89ea99d76c1358b9619b91e06dc
Dominik Kapusta <dominik@kapusta.cc>
2020-01-28 22:10:08 +0100

    Move fontdata to lib

---
+++ lib/fontdata/fontdata.h
@@ -0,0 +1,31 @@
+#ifndef FONTDATA_H
+#define FONTDATA_H
+
+#include <vector>
+
+class Glyph {
+
+public:
+    explicit Glyph(size_t width, size_t height);
+    explicit Glyph(size_t width, size_t height, std::vector<bool> pixels);
+
+    size_t width() const { return width_; }
+    size_t height() const { return height_; }
+
+private:
+    size_t width_;
+    size_t height_;
+    std::vector<bool> pixels_;
+};
+
+
+class FontFace
+{
+public:
+    FontFace() = default;
+
+private:
+    std::vector<Glyph> glyphs_;
+};
+
+#endif // FONTDATA_H
